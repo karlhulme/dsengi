00413a8980cf64d338698a32463f00a9704fa3f7
karlhulme <karlhulme@hotmail.com>
2022-06-04 11:28:04 +0100

    Added tests for repair of docOpIds.

--- src/engine/docTypes/applyCommonFieldValuesToDoc.test.ts
+++ src/engine/docTypes/applyCommonFieldValuesToDoc.test.ts
@@ -5,25 +5,28 @@ import { applyCommonFieldValuesToDoc } from "./applyCommonFieldValuesToDoc.ts";
 Deno.test("The creation properties are set if not already populated.", () => {
   const doc: DocRecord = {};
   applyCommonFieldValuesToDoc(doc, 5678, "aUser");
+  assertEquals(doc.docOpIds, []);
   assertEquals(doc.docCreatedMillisecondsSinceEpoch, 5678);
   assertEquals(doc.docCreatedByUserId, "aUser");
   assertEquals(doc.docLastUpdatedMillisecondsSinceEpoch, 5678);
   assertEquals(doc.docLastUpdatedByUserId, "aUser");
 });
 
-Deno.test("The creation properties are ignored if already set, and just updated properties are set.", () => {
+Deno.test("The creation properties are ignored if already set, and just lastUpdated properties are added.", () => {
   const doc: DocRecord = {
+    docOpIds: ["abcd"],
     docCreatedMillisecondsSinceEpoch: 1111,
     docCreatedByUserId: "originalUser",
   };
   applyCommonFieldValuesToDoc(doc, 5678, "aUser");
+  assertEquals(doc.docOpIds, ["abcd"]);
   assertEquals(doc.docCreatedMillisecondsSinceEpoch, 1111);
   assertEquals(doc.docCreatedByUserId, "originalUser");
   assertEquals(doc.docLastUpdatedMillisecondsSinceEpoch, 5678);
   assertEquals(doc.docLastUpdatedByUserId, "aUser");
 });
 
-Deno.test("The updation properties are updated with new values.", () => {
+Deno.test("The lastUpdated properties are updated with new values.", () => {
   const doc: DocRecord = {
     docCreatedMillisecondsSinceEpoch: 1111,
     docCreatedByUserId: "originalUser",
@@ -31,6 +34,7 @@ Deno.test("The updation properties are updated with new values.", () => {
     docLastUpdatedByUserId: "anotherUser",
   };
   applyCommonFieldValuesToDoc(doc, 5678, "aUser");
+  assertEquals(doc.docOpIds, []);
   assertEquals(doc.docCreatedMillisecondsSinceEpoch, 1111);
   assertEquals(doc.docCreatedByUserId, "originalUser");
   assertEquals(doc.docLastUpdatedMillisecondsSinceEpoch, 5678);
