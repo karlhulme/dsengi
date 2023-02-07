b22612310c5f6723a2fa71c0bb678ac4b9acdbed
Karl Hulme <karlhulme@hotmail.com>
2023-02-07 09:01:29 +0000

    Export mongo-doc-store.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./cosmosDocStore/index.ts";
 export * from "./engine/runtime/index.ts";
 export * from "./interfaces/index.ts";
 export * from "./memDocStore/index.ts";
+export * from "./mongoDocStore/index.ts";
